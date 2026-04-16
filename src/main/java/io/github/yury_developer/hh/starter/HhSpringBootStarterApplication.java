f8b1910f06a3585ee46a07ddb80e6f040bdd339a
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-04-16 12:15:56 +0300

    init commit

---
+++ src/main/java/io/github/yury_developer/hh/starter/HhSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.yury_developer.hh.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhSpringBootStarterApplication.class, args);
+	}
+
+}
